ddf5fc96666b9adf86760321bd6ba54e0ea81c4c
lilydjwg <lilydjwg@gmail.com>
2022-07-05 10:03:48 +0800

    update deps

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,51 @@ struct Opt {
   device: String,
 }
 
+fn show_rdata(name: &str, rdata: &RData, arrow: &str) {
+  match rdata {
+    RData::A(ip) => {
+      println!("{} {} {}", name, arrow, ip);
+    },
+    RData::AAAA(ip) => {
+      println!("{} {} {}", name, arrow, ip);
+    },
+    RData::CNAME(cname) => {
+      let s = cname.to_string();
+      let cname_str = s.trim_end_matches('.');
+      println!("{} {} {}", name, arrow, cname_str);
+    },
+
+    RData::HTTPS(svcb) => {
+      use trust_dns_proto::rr::rdata::svcb::{SvcParamKey, SvcParamValue, IpHint};
+      let has_ech = svcb.svc_params().iter()
+        .any(|(k, _)| matches!(k, SvcParamKey::EchConfig));
+      let tag = if has_ech { "HTTPS ECH" } else { "HTTPS" };
+      let mut is_first = true;
+      for (_, v) in svcb.svc_params() {
+        match v {
+          SvcParamValue::Ipv4Hint(IpHint(ips)) => {
+            for ip in ips {
+              let arrow = if is_first { "=>" } else { "->" };
+              println!("{} {} {} {}", name, arrow, tag, ip);
+              is_first = false;
+            }
+          },
+          SvcParamValue::Ipv6Hint(IpHint(ips)) => {
+            for ip in ips {
+              let arrow = if is_first { "=>" } else { "->" };
+              println!("{} {} {} {}", name, arrow, tag, ip);
+              is_first = false;
+            }
+          },
+          _ => { },
+        }
+      }
+    },
+
+    _ => { },
+  }
+}
+
 fn process(packet: &[u8]) {
   // 42 = 14 Ethernet header + 20 IPv4 header + 8 UDP header
   match Message::from_bytes(&packet[42..]) {
@@ -28,22 +73,9 @@ fn process(packet: &[u8]) {
       let mut is_first = true;
       for a in msg.answers() {
         let arrow = if is_first { "=>" } else { "->" };
-
-        match a.rdata() {
-          RData::A(ip) => {
-            println!("{} {} {}", name, arrow, ip);
-          },
-          RData::AAAA(ip) => {
-            println!("{} {} {}", name, arrow, ip);
-          },
-          RData::CNAME(cname) => {
-            let s = cname.to_string();
-            let cname_str = s.trim_end_matches('.');
-            println!("{} {} {}", name, arrow, cname_str);
-          },
-          _ => { },
+        if let Some(rdata) = a.data() {
+          show_rdata(name, rdata, arrow);
         }
-
         is_first = false;
       }
     },
@@ -59,7 +91,7 @@ fn main() -> Result<(), Error> {
 
   let mut cap = pcap::Capture::from_device(device)?
     .immediate_mode(true).open()?;
-  cap.filter("ip proto \\udp and src port 53")?;
+  cap.filter("ip proto \\udp and src port 53", true)?;
   loop {
     match cap.next() {
       Ok(packet) => process(&packet),
